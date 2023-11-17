5a3794221ac06a4798526d02042c10cfbac9d7b8
LuchoDMD <lucio.mdq10@gmail.com>
2023-11-17 15:20:58 -0300

    17/11 modificando componente registro

--- src/app/components/c_validators/pass-validator.spec.ts
+++
@@ -1,7 +0,0 @@
-import { PassValidator } from './pass-validator';
-
-describe('PassValidator', () => {
-  it('should create an instance', () => {
-    expect(new PassValidator()).toBeTruthy();
-  });
-});
