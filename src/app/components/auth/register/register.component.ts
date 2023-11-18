082dc36d5cc2f981e25ba63f59a6d659cf3781d1
LuchoDMD <lucio.mdq10@gmail.com>
2023-11-18 17:53:08 -0300

    18/11 Volviendo a empezar el Registro

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { User } from "src/app/models/classes/user";
 
 
@@ -10,12 +11,19 @@ import { User } from "src/app/models/classes/user";
   
   export class RegisterComponent implements OnInit {
     usuario: User = new User();
-    
+    f_usuario!: FormGroup;
     constructor() { }
   
     ngOnInit(): void 
     {
-      
+      this.f_usuario=new FormGroup(
+        {
+          'lName': new FormControl(this.usuario.lName,[Validators.required,Validators.maxLength(50)]),
+          'fName': new FormControl(this.usuario.fName,[Validators.required,Validators.maxLength(50)]),
+          'nName': new FormControl(this.usuario.nName,[Validators.required,Validators.maxLength(50)]),
+          'email': new FormControl(this.usuario.email,[Validators.required,Validators.email]),
+          'passw': new FormControl(this.usuario.passw,[Validators.required,Validators.maxLength(50)])
+        });
     }
   
     ngOnSubmit() 
